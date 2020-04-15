7c24f39ce595fabbbc7810a7f01e4e334dcecc96
Ellen Lee <leechinghui.tw@gmail.com>
2020-04-15 14:35:09 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
   Todo.find()
     .lean()
+    .sort({ _id: 'asc' }) // desc
     .then(todos => res.render('index', { todos }))
     .catch(error => console.error(error))
 })
