eaf2cc4fa34f3e6cfd6172ce07d16b75eeb27c07
Victor Neo <icyisamu@gmail.com>
2022-07-22 00:40:43 +0800

    Node 18 compat changes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require('./config/mongoose')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
